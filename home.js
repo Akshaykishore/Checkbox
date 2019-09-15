961ca72bead06a90d3341afddb54ec75a8e0a225
Akshay <akshayvicky96@gmail.com>
2019-09-16 00:48:48 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,8 +1,20 @@
+var ID;
 function enable_text(id)
 {
     var checkBox = document.getElementById(id.id);
-    var  textarea = document.getElementById("textarea");
     if (checkBox.checked == true){
         textarea.disabled = false;
+        ID = id.id;
     } 
+    else{
+        textarea.disabled = true;
+    }
+}
+
+function get_value()
+{
+    console.log(ID)
+    var text=document.getElementById("textarea").value;
+    var column = document.getElementById(ID+'_value');
+    column.innerHTML=text;    
 }
